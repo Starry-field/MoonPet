0da4c4b93ffcd948602a28ee84ac239d6144a006
Emotion-lmbuer <usman081814850@gmail.com>
2026-05-19 15:02:05 +0800

    init: Fabric mod template

---
+++ src/client/java/io/github/starrymoon/moonpet/client/MoonPetClient.java
@@ -0,0 +1,10 @@
+package io.github.starrymoon.moonpet.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoonPetClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
